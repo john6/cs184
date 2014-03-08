e4263cc4ebad4ce7e5c7ff0a3e06d2066a5c34b3
shanaxel42 <shanaxel42@berkeley.edu>
2014-03-08 01:14:03 -0800

    Added intersection code for spheres

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,5 @@
 //
-//  Scene.cpp
+//  Main.cpp
 //  
 //
 //  Created by Shannon Axelrod on 2/23/14.
@@ -13,6 +13,7 @@
 #include <fstream>
 #include <sstream>
 #include <list>
+#include <iterator>
 using namespace std;
 
 
@@ -22,13 +23,45 @@ using namespace std;
 //****************************************************
 class Color{
 public:
+    Eigen::Vector3f rgb_vec; 
     float r, g, b;
     Color(float _r, float _g, float _b){
         r = _r;
         g = _g;
         b = _b; 
+        Eigen::Vector3f temp(r, g, b);
+        rgb_vec = temp;  
+      
     };
     Color(){};
+    void reset(){
+            r = rgb_vec[0];
+            g = rgb_vec[1];
+            b = rgb_vec[2];
+        }
+
+};
+
+//****************************************************
+// Point
+//****************************************************
+class Point{
+public:
+    float x, y, z;
+    Eigen::Vector3f pt_vec; 
+    Point(float _x, float _y, float _z){
+        x = _x;
+        y = _y;
+        z = _z; 
+        Eigen::Vector3f temp(x, y, z);
+        pt_vec = temp; 
+    };
+    Point(){}
+    //Computes the distance between two points
+    float dist(Point other){ 
+        return sqrt(pow((x - other.x),2) + pow((y - other.y),2) + pow((z-other.z),2));
+    }
+
 };
 
 //****************************************************
@@ -46,18 +79,51 @@ public:
     Light(){}
 };
 
+//****************************************************
+// Sphere
+//****************************************************
+class Sphere{
+public:
+    Point pos;
+    float radius; 
+    Sphere(Point _pos, float _radius){
+        pos = _pos;
+        radius = _radius;
+    }
+    Sphere(){}
+
+};
+
+//****************************************************
+// Tri
+//****************************************************
+class Tri{
+public:
+    Point v1, v2, v3;
+    Tri(Point _v1, Point _v2, Point _v3){
+        v1 = _v1;
+        v2 = _v2;
+        v3 = _v3; 
+    }
+    Tri(){}
+};
 
 //****************************************************
 // Global Variabels
 //****************************************************
 //Camera myCamera;
-int height, width, depth;
+int height, width, depth = 5, threshhold;
 Color ambientColor, specularColor, emissionColor, diffuseColor; 
+Color black_pix(0.0, 0.0, 0.0);
+float sp_v;
 string out_file; 
 int maxvertsnorms;
 const int DIR = 1;
 const int POINT = 0; 
-list<Light> AllLights; 
+Light AllLights[10]; int numLights; 
+Sphere AllSpheres[100]; int numSpheres;
+Tri AllTri[100]; int numTri;
+Point vertices[100]; int numVerts;
 
 //****************************************************
 // Camera
@@ -68,7 +134,7 @@ public:
               vert_length, hor_length, pixel_length, half_pixel_offset, FOVY;
 
     Eigen::Vector3f look_from, look_at, up, right, center_screen_coord, top_left_c, top_right_c, 
-                    bottom_left_c, bottom_right_C;
+                    bottom_left_c, bottom_right_c;
     Camera(){};
    
   Camera(Eigen::Vector3f eye, Eigen::Vector3f view_dir, Eigen::Vector3f up_dir, float field_of_view){
@@ -93,6 +159,7 @@ public:
        half_pixel_offset = pixel_length/2;
 
    }
+   
 
 
 
@@ -100,20 +167,6 @@ public:
     
 };
 
-//****************************************************
-// Point
-//****************************************************
-class Point{
-public:
-    float x, y, z;
-    Point(int _x, int _y, int _z){
-        x = _x;
-        y = _y;
-        z = _z; 
-    };
-    Point(){}
-
-};
 
 //****************************************************
 // Ray
@@ -133,14 +186,15 @@ public:
 };
 
 
+
 //****************************************************
 // GenerateRay
 //****************************************************
 Ray GenerateRay(Camera cam, int pix_i, int pix_j, int height, int width){
     //THIS IS WHERE WE SHOULD DO THOSE DANK EQUATIONS
        //  P = (u)(vLL+ (1!v)UL) + (1!u)(vLR+ (1!v)UR)
-   float dist_from_left_side = cam.half_pixel_offset + cam.pixel_length * pix_i; //this could be fucking j for all I know right now
-   float dist_from_top = cam.half_pixel_offset + cam.pixel_length * pix_j; //this could be fucking i for all I know right now
+   float dist_from_left_side = cam.half_pixel_offset + cam.pixel_length * pix_i; 
+   float dist_from_top = cam.half_pixel_offset + cam.pixel_length * pix_j; 
 
 
     Eigen::Vector3f u(0,0,0);
@@ -155,22 +209,31 @@ Ray GenerateRay(Camera cam, int pix_i, int pix_j, int height, int width){
 //****************************************************
 Eigen::Vector3f ambientTerm(Color color, Light myLight){
     Eigen::Vector3f ambient = Eigen::Vector3f(color.r, color.g, color.b);
-    ambient[0] = ambientColor.r*myLight.LightRGB.r;
-    ambient[1] = ambientColor.g*myLight.LightRGB.g;
-    ambient[2] = ambientColor.b*myLight.LightRGB.b;
+    ambient = ambient.cross(myLight.LightRGB.rgb_vec);
     return ambient;
 }
 
 Eigen::Vector3f diffuseTerm(Color color, Light myLight, Eigen::Vector3f surf_norm){
-     Eigen::Vector3f d_color; 
+    float d;
+    d = myLight.LightVector.dot(surf_norm);
+    Eigen::Vector3f d_color(color.r, color.g, color.b);
+    //take the max of kd an0
+    d = fmax(d, 0);
+    d_color = d * d_color;
+    //multiply Kd by the intesity
+    d_color = d_color.cross(myLight.LightRGB.rgb_vec);
 
      return d_color; 
 }
 
 Eigen::Vector3f specularTerm(Color color, Eigen::Vector3f surf_normal, Light myLight, Eigen::Vector3f viewVector){
     Eigen::Vector3f r_vec = (2*(myLight.LightVector.dot(surf_normal))*surf_normal) - myLight.LightVector;
-
-    return r_vec; 
+    r_vec.normalize();
+    Eigen::Vector3f s_color(color.r, color.g, color.b);
+    float r_dotV = r_vec.dot(viewVector);
+    s_color = s_color* pow(fmax(r_dotV, 0), sp_v);
+    s_color = s_color.cross(myLight.LightRGB.rgb_vec);
+   return s_color; 
 }
 
 
@@ -205,31 +268,40 @@ bool parseLine(string line){
         height = values[1];
     }else if(operand.compare("maxdepth") == 0){
         ss>>depth; 
+        printf("%d\n", depth );
     }else if(operand.compare("output") == 0){
         ss>>out_file; 
 
     //CAMERA
     }else if (operand.compare("camera") == 0){
          readvals(ss, 10, values);
-     //    myCamera.look_from = Eigen::Vector3f(values[0], values[1], values[2]);
-       //  myCamera.look_at = Eigen::Vector3f(values[3], values[4], values[5]);
-       //  myCamera.up = Eigen::Vector3f(values[6], values[7], values[8]);
-        // myCamera.FOVY = values[9];
+         Camera temp(Eigen::Vector3f(values[0], values[1], values[2]), 
+                     Eigen::Vector3f(values[3], values[4], values[5]), 
+                     Eigen::Vector3f(values[6], values[7], values[8]),
+                     values[9]);
 
     //GEOMETRY
     }else if (operand.compare("sphere") == 0){
         readvals(ss, 4, values);
+        Sphere newSphere(Point(values[0], values[1], values[2]), values[4]);
+        numSpheres++;
 
     }else if (operand.compare("maxverts") == 0){
         ss>>maxvertsnorms;
     }else if (operand.compare("vertex") == 0){
         readvals(ss, 3, values);
+        Point newVertex(values[0], values[1], values[2]);
+        vertices[numVerts] = newVertex;
+        numVerts++; 
 
     }else if (operand.compare("vertexnormal") == 0){
         readvals(ss, 6, values);
 
     }else if (operand.compare("tri") == 0){
-        readvals(ss, 3, values);
+        readvals(ss, 3 ,values);
+        Tri newTri(vertices[(int)values[0]], vertices[(int)values[1]], vertices[(int)values[2]]);
+        numTri++;
+
     }else if (operand.compare("trinormal") == 0){
         readvals(ss, 3, values);
 
@@ -250,13 +322,15 @@ bool parseLine(string line){
         readvals(ss, 6, values);
         Light new_DirLight(DIR, Eigen::Vector3f(values[0], values[1], values[2]), 
                                 Color(values[3], values[4], values[5]));
-        AllLights.push_front(new_DirLight);
+        AllLights[numLights] = new_DirLight;
+        numLights++;
 
     }else if (operand.compare("point") == 0){
         readvals(ss, 6, values);
         Light new_pointLight(POINT, Eigen::Vector3f(values[0], values[1], values[2]), 
                                     Color(values[3], values[4], values[5]));
-        AllLights.push_front(new_pointLight);
+        AllLights[numLights] = new_pointLight;
+        numLights++;
 
     }else if (operand.compare("attenuation") == 0){
         readvals(ss, 3, values);
@@ -276,7 +350,7 @@ bool parseLine(string line){
         Color temp2(values[0], values[1], values[2]);
         specularColor= temp2;
     }else if (operand.compare("shininess") == 0){
-        readvals(ss, 1, values);
+        ss>>sp_v;
     }else if (operand.compare("emmission") == 0){
         readvals(ss, 3, values);
         Color temp3(values[0], values[1], values[2]);
@@ -301,6 +375,95 @@ void parseScene(string filename){
     inFile.close(); 
 }
 
+//****************************************************
+// Intersections
+//****************************************************
+bool sphereIntersection(Sphere sphere, Ray ray){
+    Eigen::Vector3f p0 = ray.pos.pt_vec; 
+    Eigen::Vector3f p1 = ray.dir; 
+    Eigen::Vector3f d_vec = p1 - p0; 
+    Eigen::Vector3f center = sphere.pos.pt_vec; 
+    float discriminant; 
+
+    float a = d_vec.dot(d_vec);
+    float b = ((p0 - center)*2).dot(d_vec);
+    float c = center.dot(center) + p0.dot(p0) + (-2*(center.dot(p0))) - pow(sphere.radius, 2); 
+    discriminant = pow(b,2) - (4*a*c);
+    if(discriminant >= 0){
+        return true; 
+    }
+    return false; 
+}
+
+
+
+//****************************************************
+// The Trace Function 
+//****************************************************
+
+Color Trace(Ray ray, int depth) {
+   
+   bool intersection = false; 
+   Color returnColor;
+ //   if (depth > threshhold) {
+   //     return black_pix;
+  // }
+        for(int i; i<numSpheres; i++){
+            if((sphereIntersection(AllSpheres[i], ray))){
+                intersection = true;
+                break;
+            }
+            /*
+            if(!intersection){  //if there was a sphere intersection already no need to check this
+                for(int j; j<numTri; j++){
+                    if(INTERSECTION WITH TR ){
+                        intersection = true;
+                        break; 
+                    }
+                }
+                */
+            }
+    if (!intersection) {
+        // No intersection
+        return black_pix;
+    }
+    /*
+        // Obtain the brdf at intersection point
+        in.primitive->getBRDF(in.local, &brdf);
+        
+        // There is an intersection, loop through all light source
+        */
+        for (int i=0; i<numLights; i++) {
+           // lights[i].generateLightRay(in.local, &lray, &lcolor);
+            Light MyLight = AllLights[i]; 
+            // Check if the light is blocked or not
+           // if (!primitive->intersectP(lray))
+                Eigen::Vector3f temp;
+                Eigen::Vector3f surface_Normal; 
+                Eigen::Vector3f viewVector(0, 0, 1.0);
+                Eigen::Vector3f ambient = ambientTerm(ambientColor, MyLight);
+                Eigen::Vector3f diffuse = diffuseTerm(diffuseColor, MyLight, surface_Normal);
+                Eigen::Vector3f specular = specularTerm(specularColor, surface_Normal, MyLight, viewVector);
+                temp = ambient + diffuse + specular;
+                returnColor.rgb_vec = returnColor.rgb_vec.cross(temp); 
+        }
+        /*
+        
+        // Handle mirror reflection
+        if (brdf.kr > 0) {
+            reflectRay = createReflectRay(in.local, ray);
+            
+            // Make a recursive call to trace the reflected ray
+            trace(reflectRay, depth+1, &tempColor);
+            *color += brdf.kr * tempColor;
+        }
+        
+        }
+        */
+        returnColor.reset(); 
+        return returnColor; 
+}
+
 
 //****************************************************
 // The Rendering Loop
@@ -313,10 +476,11 @@ Color** render(Camera camera, int height, int width) {
 
     for (int i= 0; i<width; i++) {
         for (int j = 0; j<height; j++) {
-            Ray ray = GenerateRay(camera, i, j, height, width);
-           // Color color = RayTracer.Trace(ray, height, width, 0, i, j);
-           // buffer[i][j] = color; 
-            buffer[i][j] = Color(i/(float)height, j/(float)width, 0);
+            // Ray ray = GenerateRay(camera, i, j, height, width);
+            Ray ray;
+            Color new_color = Trace(ray, depth);
+            buffer[i][j] = new_color; 
+           // buffer[i][j] = Color(i/(float)height, j/(float)width, 0);
         }
     }
     return buffer; 
@@ -374,26 +538,10 @@ int main(int argc, char* argv[]){
 
     parseScene(argv[1]);
 
-       //setting params to specific things just to test
-   height = 10;
-   width = 10;
-   Eigen::Vector3f eye = Eigen::Vector3f(0,3,0);
-   Eigen::Vector3f view_dir = Eigen::Vector3f(1,0,0);
-   Eigen::Vector3f up_dir = Eigen::Vector3f(0,1,0);
-   float fovY = 0.78539816339; //  pi/4
-
-     // set up camera
-   //Camera(Eigen::Vector3f eye, Eigen::Vector3f view_dir, Eigen::Vector3f up_dir, float field_of_view)
-   Camera cam = Camera(eye, view_dir, up_dir, fovY);
-   //printf("float: %f", floatValue);
-   printf("\ntop_left_c: %f, %f, %f", cam.top_left_c(0), cam.top_left_c(1), cam.top_left_c(2));
-   printf("\ntop_right_c: %f, %f, %f", cam.top_right_c(0), cam.top_right_c(1), cam.top_right_c(2));
-   printf("\nbottom_left_c: %f, %f, %f", cam.bottom_left_c(0), cam.bottom_left_c(1), cam.bottom_left_c(2));
-   printf("\nbottom_right_C: %f, %f, %f", cam.bottom_right_C(0), cam.bottom_right_C(1), cam.bottom_right_C(2));
    
-    
-   // Color** testbuffer = render(myCamera, height, width);
-   // outputFrame(testbuffer);
+    Camera myCamera;
+    Color** testbuffer = render(myCamera, height, width);
+    outputFrame(testbuffer);
     
     return 0;
 }
